1aa89f7a30b33a68567928fe5ed1ffe3e6d22e30
Swarna Akter <124721243+swarna-M08@users.noreply.github.com>
2024-01-05 00:03:21 -0800

    Time: 0 ms (100.00%), Space: 6.3 MB (43.05%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        int result=1;
        for(int i=0;i<=30;i++){
-           int result = pow(2,i);
            if(result==n){
                return true;
            }
+           if(result < INT_MAX/2)
+               result = result*2;    
+               
        }
         return false;
     }
