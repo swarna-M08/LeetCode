291b3b7f742895643c4343cbe1095c8a86add74a
Swarna Akter <124721243+swarna-M08@users.noreply.github.com>
2024-01-16 05:08:03 -0800

    Time: 2 ms (77.06%), Space: 46.2 MB (15.99%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,8 +3,7 @@ class Solution {
         int count=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                nums[count]=nums[i];
-                count++;
+                nums[count++]=nums[i];
             }
         }
         while(count<nums.length){
